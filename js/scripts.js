f37ec4f6e60fbc880bafaf07730bd6eae91df0fe
Melissa <melissa.scmi@gmail.com>
2021-08-16 15:25:23 -0700

    add function to push item to list in progress
    Co-authored-by: Art Weinstein <artur.weinstein@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  let list = [];
+  console.log(list);
+  console.log("hello??");
+  $("#addItem").click(function(event){
+    event.preventDefault();
+    let groceryItem = $("input#groceryItem").val();
+    list.push(groceryItem);
+    console.log(list);
+   $("#listProgress").append(list); 
+  });
+  
+
+});
+
+
+
+
+
+// $(document).ready(function(){
+  //   $("#favoriteList").click(function(){
+  // const favIceCream = ["Chocolate", "Vanilla", "Peppermint", "Cotton candy"];
+  // console.log(favIceCream);
+
+  // let favFood = $("input#favFood").val();
+  // let favMusic = $("input#favMusic").val();
+  // let favHair = $("input#favHair").val();
+  // let favThings = [];
+  // let reverseFavs = [];
+  //   favThings.push(favFood);
+  //   favThings.push(favMusic);
+  //   favThings.push(favHair);
+  //   favThings.push("Chicken");
